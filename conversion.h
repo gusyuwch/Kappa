6d5be718e8f7343335302f947b25c0371d662af2
gusyuwch <mhlwjsso001@yahoo.cn>
2012-03-30 16:23:22 +0800

    uploadt

---
+++ conversion.h
@@ -0,0 +1,6 @@
+float conversion_ktp(void);
+float conversion_ktb(void);
+float conversion_ptk(float psi);
+float conversion_ptb(float psi);
+float conversion_btk(float bar);
+float conversion_btp(float bar);
